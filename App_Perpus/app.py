a2f394ab9499d2900c040454692e59aabca3c582
chrisnaocta <chrisnaoctavianus@gmail.com>
2024-06-10 17:50:18 +0700

    Tambah data buku

--- App_Perpus/app.py
+++ App_Perpus/app.py
@@ -199,6 +199,35 @@ def staff_logout():
     session.pop('id', None)
     return redirect(url_for('home'))
 
+@application.route('/staff/collection/tambah/', methods=['GET','POST'])
+def staff_tambah_buku():
+    if 'id' not in session:
+        return redirect(url_for('home'))
+    if 'staff_forgot' in session:
+        return redirect(url_for('staff_forgot_entry'))
+    
+    generated_id_buku = generate_id_buku()  # Memanggil fungsi untuk mendapatkan NIK otomatis
+    
+    if request.method == 'POST':
+        id_buku = request.form['id_buku']
+        judul = request.form['judul']
+        penulis = request.form['penulis']
+        penerbit = request.form['penerbit']
+        tglterbit = request.form['tglterbit']
+        lokasi = request.form['lokasi']
+        jmlhtersedia = request.form['jmlhtersedia']
+        tglmasuk = request.form['tglmasuk']
+
+        openDb()
+        sql = "INSERT INTO buku (id_buku,judul,penulis,penerbit,tglterbit,lokasi,jmlhtersedia,tglmasuk) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (id_buku,judul,penulis,penerbit,tglterbit,lokasi,jmlhtersedia,tglmasuk)
+        cursor.execute(sql, val)
+        conn.commit()
+        closeDb()
+        return redirect(url_for('staff_dashboard'))        
+    else:
+        return render_template('staff_tambah_buku.html', id_buku=generated_id_buku)  # Mengirimkan NIK otomatis ke template
+
 #fungsi cetak ke PDF
 @application.route('/print/<id_buku>', methods=['GET'])
 def get_employee_data(id_buku):
@@ -267,22 +296,22 @@ class Users(db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash)
 
-#fungsi membuat ID otomatis
+#fungsi membuat ID staff otomatis
 def generate_id():
     # mendefinisikan fungsi openDb(), cursor, dan closeDb() 
     openDb()
 
-    current_year = 678
-    current_month = 123
+    tiga_digit_pertama = 678
+    tiga_digit_kedua = 123
     
     # Mengambil empat digit terakhir dari tahun
-    year_str = str(current_year).zfill(2)
+    year_str = str(tiga_digit_pertama).zfill(2)
     
     # Mengambil dua digit dari bulan
-    current_month_str = str(current_month).zfill(2)
+    tiga_digit_str_kedua = str(tiga_digit_kedua).zfill(2)
 
     # Membuat format nia tanpa nomor urut terlebih dahulu
-    base_id_without_number = f"I-{year_str}{current_month_str}"
+    base_id_without_number = f"I-{year_str}{tiga_digit_str_kedua}"
 
     # Mencari nia terakhir dari database untuk mendapatkan nomor urut
     cursor.execute("SELECT id FROM staff WHERE id LIKE %s ORDER BY id DESC LIMIT 1", (f"{base_id_without_number}%",))
@@ -293,6 +322,42 @@ def generate_id():
         next_number = last_number + 1
         # Membuat nia lengkap dengan nomor urut
         next_id = f"I-{str(next_number).zfill(3)}"
+    else:
+        next_number = 1  # Jika belum ada data, mulai dari 1
+        # Membuat nia lengkap dengan nomor urut
+        next_id = f"{base_id_without_number}{str(next_number).zfill(3)}"
+    
+    closeDb()  # untuk menutup koneksi database 
+    
+    return next_id
+
+#fungsi membuat ID buku otomatis
+def generate_id_buku():
+    # mendefinisikan fungsi openDb(), cursor, dan closeDb() 
+    openDb()
+
+    tiga_digit_pertama = 336
+    # digit_keempat = 1
+    
+    # Mengambil empat digit terakhir dari tahun
+    first_format = str(tiga_digit_pertama).zfill(2)
+    
+    # Mengambil dua digit dari bulan
+    # tiga_digit_str_kedua = str(digit_keempat).zfill(2)
+
+    # Membuat format nia tanpa nomor urut terlebih dahulu
+    base_id_without_number = f"B-{first_format}"
+    # base_id_without_number = f"B-{first_format}{tiga_digit_str_kedua}"
+
+    # Mencari nia terakhir dari database untuk mendapatkan nomor urut
+    cursor.execute("SELECT id_buku FROM buku WHERE id_buku LIKE %s ORDER BY id_buku DESC LIMIT 1", (f"{base_id_without_number}%",))
+    last_id_buku = cursor.fetchone()
+
+    if last_id_buku:
+        last_number = int(last_id_buku[0].split("-")[-1])  # Mengambil nomor urut terakhir
+        next_number = last_number + 1
+        # Membuat nia lengkap dengan nomor urut
+        next_id = f"B-{str(next_number).zfill(3)}"
     else:
         next_number = 1  # Jika belum ada data, mulai dari 1
         # Membuat nia lengkap dengan nomor urut
